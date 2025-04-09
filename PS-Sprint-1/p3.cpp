948279c671f62cb35291b21db05de6df2d45232b
Mangesh Nesarikar <150088840+xxMangeshxx@users.noreply.github.com>
2025-04-09 15:40:38 +0530

    Create p3.cpp

---
+++ PS-Sprint-1/p3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cout<<"Enter the year:";
+    cin>>year;
+    if(year%400==0){
+      cout<<"It is a leap year\n";
+    }
+  else if(year%100==0){
+      cout<<"It is not a leap year\n";
+  
+  }
+  else if(year%4==0){
+      cout<<"It is a leap year\n";
+  }
+  else {
+      cout<<"It is not a leap year\n";
+  }
+
+    return 0;
+}
