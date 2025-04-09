314b1ed2030d80dd5286b5ebcd1ab547aaa5b550
Mangesh Nesarikar <150088840+xxMangeshxx@users.noreply.github.com>
2025-04-10 04:37:48 +0530

    Create p4.cpp

---
+++ PS-Sprint-1/p4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  int x,n,m;
+  cout<<"Enter the nunber to find if it's an Amstrong number:";
+  cin>>x;
+  n=x;
+  m=x;
+  int numofdigits=0;
+  int rem,sum=0;
+  while (n>0){
+    n=n/10;
+    numofdigits++;
+  }
+  while (m>0){
+    rem=m%10;
+    sum+=pow(rem,numofdigits);
+    m/=10;
+  }
+  if(x==sum){
+    cout<<"It is an Armstrong number";
+  }
+  else{
+    cout<<"It is not an Armstrong number";
+  }
+  return 0;
+}
