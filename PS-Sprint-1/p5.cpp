be36c3f7d377519259b3fa9e072daee38cbc7c50
Mangesh Nesarikar <150088840+xxMangeshxx@users.noreply.github.com>
2025-04-10 16:53:05 +0530

    Create p5.cpp

---
+++ PS-Sprint-1/p5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int l, first=0,second=1, third;
+  cout<<"Enter the limit uo to which Fibonacci numbers should be generated:";
+  cin>>l;
+  cout<<first<<endl;
+  if(second<=l)
+  cout<<second<<endl;
+  while (second<=l){
+    third=first+second;
+    if(third<=l){
+      cout<<third<<endl;
+    }
+    first=second;
+    second=third;
+  }
+  
+  return 0;
+  
+}
